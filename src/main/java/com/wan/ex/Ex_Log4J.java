2e9915d5700107dfacc549b4856634538731ca42
wanhailang <769138655@qq.com>
2020-04-11 20:22:34 +0800

    2020.04.11

--- src/main/java/com/wan/ex/Ex_Log4J.java
+++ src/main/java/com/wan/ex/Ex_Log4J.java
@@ -1,8 +1,20 @@
 package com.wan.ex;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.helpers.OptionConverter;
+
 public class Ex_Log4J {
 
     public static void main(String[] args) {
         System.out.println("Ex Log4J Test ...");
+
+        System.out.println(OptionConverter.getSystemProperty(
+                "log4j.configuratorClass",
+                null));
+
+        Logger logger = Logger.getLogger(Ex_Log4J.class);
+
+        logger.info("");
+
     }
 }
