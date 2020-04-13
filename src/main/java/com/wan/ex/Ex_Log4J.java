efaf89ff515dcf8d108dfdfeb14ec3243ff5c4d0
wanhailang <769138655@qq.com>
2020-04-13 22:42:17 +0800

    2020.04.13 log4j properties

--- src/main/java/com/wan/ex/Ex_Log4J.java
+++ src/main/java/com/wan/ex/Ex_Log4J.java
@@ -8,13 +8,15 @@ public class Ex_Log4J {
     public static void main(String[] args) {
         System.out.println("Ex Log4J Test ...");
 
-        System.out.println(OptionConverter.getSystemProperty(
-                "log4j.configuratorClass",
-                null));
-
         Logger logger = Logger.getLogger(Ex_Log4J.class);
 
-        logger.info("");
+        logger.info("Logger Info Test ...");
+
+        logger.error("Logger Error Test ...");
+
+        logger.debug("Logger Debug Test ...");
+
+        
 
     }
 }
